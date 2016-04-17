2aab3deb2cd8d6ff9f2308ceb9a1e0ce0cd23412
NeedSomeMeat <htc.x.boy@gmail.com>
2016-04-17 15:13:20 +0300

    [Update] updated key for NYT api and changed path to json

--- www/modules/Profile/ProfileService.js
+++ www/modules/Profile/ProfileService.js
@@ -46,9 +46,9 @@
         function getNews(){
             return $http ({
                 method: 'GET',
-                url: 'http://api.nytimes.com/svc/movies/v2/reviews/all?',
+                url: 'http://api.nytimes.com/svc/movies/v2/reviews/all.json?order=by-publication-date&',
                 params:{
-                    'api-key': 'e66915c32b9bd02d7bb83d338949f493:7:74820590'
+                    'api-key': 'af40a746c0d21cbbfa400595a78f03ab:10:74820590'
                 }
             }).then(function(response){
                 return response.data;
